6557ceb55864544cbabe86bab1c2931207ac51e0
DESKTOP-QFUJBHK\Marina <marinasolovieva189@gmail.com>
2023-07-04 11:21:51 +0300

    add exception handler

---
+++ src/main/java/com/example/estote/controller/GlobalExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.example.estote.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler (Exception.class)
+    public ModelAndView handleException(Exception ex) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("error");
+        modelAndView.addObject("errorMessage", "Произошла ошибка: " + ex.getMessage());
+        return modelAndView;
+    }
+}
